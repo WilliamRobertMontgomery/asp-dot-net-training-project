90d4ac9dd1a510ce56747ddadc19c14564a751fb
EdikTrutsik <EdikTrutsik@gmail.com>
2012-03-15 11:50:21 +0000

    Eduard Trutsik add NUnit Tests

---
+++ Eduard.Trutsik/My.Complex/My.Complex.Tests/Runner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Complex.Tests
+{
+    class Runner
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            NUnit.ConsoleRunner.Runner.Main(args);
+        }
+    }
+}
