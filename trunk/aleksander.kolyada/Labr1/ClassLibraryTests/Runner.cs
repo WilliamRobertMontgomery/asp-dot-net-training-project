2f4de60f75d84bf28aa266a62dd54c99a1ef044c
Aleksander.Kolyada@gmail.com <Aleksander.Kolyada@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-15 10:14:55 +0000

    LABR1(TDD)

---
+++ trunk/aleksander.kolyada/Labr1/ClassLibraryTests/Runner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibraryTests
+{
+    class Runner
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            int returnCode; 
+            returnCode = NUnit.ConsoleRunner.Runner.Main(args);
+            if (returnCode != 0)
+            {
+                Console.Beep();
+            }
+        }
+    }
+}
