e51e13c5edd83f7a42f7eff2c0ee1ded0110632e
Aleksander.Kolyada <Aleksander.Kolyada@gmail.com>
2012-03-15 10:14:55 +0000

    LABR1(TDD)

---
+++ aleksander.kolyada/Labr1/ClassLibrary/Vector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Vector
+    {
+       private double x, y; //vector coordinates
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double GetX()
+        {
+            return x;
+        }
+
+        public double GetY()
+        {
+            return y;
+        }
+
+        public static Vector Sum(Vector A, Vector B)
+        {
+            Vector C = new Vector(A.x+B.x,A.y+B.y);
+            return C;
+        }
+    }
+}
---
+++ aleksander.kolyada/Labr1/ClassLibraryTests/VectorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ClassLibrary;
+
+namespace ClassLibraryTests
+{
+    [TestFixture]
+    public class VectorTests
+    {
+        [Test]
+        public void TestSum()
+        {
+            Vector a = new Vector(1, 2), b = new Vector(2, 3), c;
+            c = Vector.Sum(a, b);
+            double actualX = c.GetX();
+            double expectedX = 3;
+            double actualY = c.GetY();
+            double expectedY = 5;
+            Assert.AreEqual(expectedX, actualX);
+            Assert.AreEqual(expectedY, actualY);
+        }
+    }
+}
