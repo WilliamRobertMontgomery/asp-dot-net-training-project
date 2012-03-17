e492870de78d90ed27a510b7dcd669fae0b8f75a
EdikTrutsik@gmail.com <EdikTrutsik@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-17 12:59:39 +0000

    Added Workshop Day2 1.11

---
+++ trunk/Eduard.Trutsik/My.Animals/My.Animals/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Animals
+{
+	class Dog :Pet
+	{
+		public Dog(string name)
+		{
+			this.name = name;
+		}
+
+		public Dog():base("Dog")
+		{
+
+		}
+
+		public void LikeEat()
+		{
+			Console.WriteLine("I like bones.");
+		}
+
+		public void Voise()
+		{
+			Console.WriteLine("I am a dog.");
+		}
+ 
+	}
+}
---
+++ trunk/Eduard.Trutsik/My.Animals/My.Animals/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Animals
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IAnimal animalDog = new Dog("AnimalDog");
+			Pet petDog = new Dog("PetDog");
+			Dog dog = new Dog("Dog");
+			animalDog.Voice();
+			animalDog.Sleep();
+			Console.WriteLine();
+
+			petDog.Voice();
+			petDog.Walk();
+			petDog.Play();
+			Console.WriteLine("Hungry: {0}",petDog.isHungry());
+			petDog.Sleep();
+			Console.WriteLine();
+
+			dog.Voice();
+			dog.LikeEat();
+			dog.Walk();
+			dog.Play();
+			Console.WriteLine("Hungry: {0}", dog.isHungry());
+			dog.Sleep();
+
+			Console.ReadKey();
+		}
+	}
+}
