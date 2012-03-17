7e6b0cb1bba7b6edcb6d99f8e72e64c5e7df84d0
OlegKlevzhic <OlegKlevzhic@gmail.com>
2012-03-17 14:29:11 +0000

    Added WorckShop2 job 1.1

---
+++ OlegKlevzhic/My.Calculator/My.Calculator/ComplexNumbers/Complex.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Calculator.ComplexNumbers
+{
+	/// <summary>
+	/// The structure is a complex number.
+	/// </summary>
+	public struct Complex<T>
+	{
+		private T re;
+		private T im;
+
+		/// <summary>
+		/// Gets or sets the value of the real part of complex number.
+		/// </summary>
+		public T Re
+		{
+			get { return re; }
+			set { re = value; }
+		}
+		
+		/// <summary>
+		/// Gets or sets the value of the imaginary part of complex number.
+		/// </summary>
+		public T Im
+		{
+			get { return im; }
+			set { im = value; }
+		}
+
+		public Complex(T re, T im)
+		{
+			this.re = re;
+			this.im = im;
+		}
+
+		public override string ToString()
+		{
+			if (re.Equals(0) && im.Equals(0))
+			{
+				return "0";
+			}
+			else
+				if (re.Equals(0))
+				{
+					return "i" + im.ToString();
+				}
+				else
+					if (im.Equals(0))
+					{
+						return re.ToString();
+					}
+					else
+						if (im.Equals(0))
+						{
+							return re.ToString() + "i" + im.ToString();
+						}
+						else
+						{
+							return re.ToString() + "+" + "i" + im.ToString();
+						}
+		}
+
+		public override bool Equals(System.Object _object)
+		{
+			if (_object == null)
+			{
+				return false;
+			}
+
+			return (re.Equals(((Complex<T>)(_object)).re)) && (im.Equals(((Complex<T>)(_object)).im));
+		}
+
+		public bool Equals(Complex<T> complexNumber)
+		{
+			if (complexNumber.Equals(null))
+			{
+				return false;
+			}
+
+			return (re.Equals(complexNumber.re)) && (im.Equals(complexNumber.im));
+		}
+
+		public override int GetHashCode()
+		{
+			return (this.re.GetHashCode() ^ (this.im.GetHashCode() << 1));
+		}
+
+	}
+}
---
+++ OlegKlevzhic/My.WorckShop.Day2/My.WorckShop.Day2/Persons.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.WorckShop.Day2
+{
+	public abstract class Person
+	{
+		private string firstName;
+		private string lastName;
+		private int age;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+		public abstract bool IsHungry();
+
+		public abstract void IntroduceYourself(Person person);
+	}
+
+	public class ConcretePerson : Person
+	{
+		public ConcretePerson()
+		{
+
+		}
+
+		public ConcretePerson(string firstName, string lastName, int age)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.Age = age;
+		}
+
+		public override string ToString()
+		{
+			return "My name is " + FirstName.ToString() + " " + LastName.ToString() + ", I am " + Age.ToString() + " years old.";
+		}
+
+		public override bool IsHungry()
+		{
+			if (new Random().Next(1, 5) == 1)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public override void IntroduceYourself(Person person)
+		{
+			if (person.IsHungry() == false)
+			{
+				Console.WriteLine(person.ToString() + "\nI am not hungry!");
+			}
+			else
+			{
+				Console.WriteLine(person.ToString() + "\nI am hungry!");
+			}
+
+		}
+	}
+
+	public class StudentConcretePerson : ConcretePerson
+	{
+		private int rate;
+
+		public int Rate
+		{
+			get { return rate; }
+			set { rate = value; }
+		}
+
+		public StudentConcretePerson()
+		{
+
+		}
+
+		public StudentConcretePerson(string firstName, string lastName, int age, int rate)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.Age = age;
+			this.Rate = rate;
+		}
+
+		public override bool IsHungry()
+		{
+			return true; // Student is always hungry :)
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + " I'm a student. I am a " + Rate.ToString() + " course.";
+		}
+	}
+
+	public class ExtramuralStudentConcretePerson : StudentConcretePerson
+	{
+		public ExtramuralStudentConcretePerson()
+		{
+
+		}
+
+		public ExtramuralStudentConcretePerson(string firstName, string lastName, int age, int rate)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.Age = age;
+			this.Rate = rate;
+		}
+
+		public override string ToString()
+		{
+			return base.ToString() + " I'm a extramural.";
+		}
+
+		public override bool IsHungry()
+		{
+			if (new Random().Next(1, 8) == 1)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
