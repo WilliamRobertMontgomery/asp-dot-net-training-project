9b272866179a2756a4b41c032b4091217fcdfc38
OlegKlevzhic@gmail.com <OlegKlevzhic@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-17 14:29:11 +0000

    Added WorckShop2 job 1.1

---
+++ trunk/OlegKlevzhic/My.Calculator/My.Calculator/Matrices/Matrix.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Calculator.Matrices
+{
+	/// <summary>
+	/// The class a matrix.
+	/// </summary>
+	public class Matrix<T>
+	{
+		int rowCount;
+		int columnCount;
+		T[,] array;
+
+		/// <summary>
+		/// Gets or sets of elements in the matrix.
+		/// </summary>
+		public T[,] Array
+		{
+			get { return array; }
+			set { array = value; }
+		}
+
+		/// <summary>
+		/// Get number of rows in the matrix.
+		/// </summary>
+		public int RowCount
+		{
+			get { return rowCount; }
+		}
+
+		/// <summary>
+		/// Get number of columns in the matrix
+		/// </summary>
+		public int ColumnCount
+		{
+			get { return columnCount; }
+		}
+
+		public Matrix(int rowCount, int columnCount)
+		{
+			if (rowCount < 0)
+			{
+				throw new ArgumentOutOfRangeException("rowCount");
+			}
+			if (columnCount < 0)
+			{
+				throw new ArgumentOutOfRangeException("columnCount");
+			}
+
+			this.array = new T[rowCount, columnCount];
+			this.rowCount = rowCount;
+			this.columnCount = columnCount;
+		}
+
+		public Matrix(int rowCount, int columnCount, T[,] array)
+		{
+			if (rowCount < 0)
+			{
+				throw new ArgumentOutOfRangeException("rowCount");
+			}
+			if (columnCount < 0)
+			{
+				throw new ArgumentOutOfRangeException("columnCount");
+			}
+
+			this.array = array;
+			this.rowCount = rowCount;
+			this.columnCount = columnCount;
+		}
+
+		public override string ToString()
+		{
+			string s = String.Empty;
+			for (int i = 0; i < rowCount; i++)
+			{
+				s += "\n";
+				for (int j = 0; j < columnCount; j++)
+				{
+					s += array[i, j].ToString() + " ";
+				}
+			}
+			return s;
+		}
+
+		public override bool Equals(System.Object _object)
+		{
+			if (_object == null)
+			{
+				return false;
+			}
+
+			return (array.Equals(((Matrix<T>)(_object))));
+		}
+
+		public bool Equals(Matrix<T> matrix)
+		{
+			if (matrix.ColumnCount != columnCount)
+			{
+				return false;
+			}
+			else if (matrix.RowCount != rowCount)
+			{
+				return false;
+			}
+			else
+			{
+				for (int i = 0; i < rowCount; i++)
+				{
+					for (int j = 0; j < columnCount; j++)
+					{
+						if (!matrix.array[i, j].Equals(array[i, j]))
+						{
+							return false;
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			return (this.array.GetHashCode() << 1);
+		}
+	}
+}
---
+++ trunk/OlegKlevzhic/My.WorckShop.Day2/My.WorckShop.Day2.Application/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace My.WorckShop.Day2.Application
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person = new ConcretePerson("Sergey", "Sergeev", 2);
+			person.IntroduceYourself(person);
+			Console.WriteLine();
+
+			person.Age = 30;
+			person.IntroduceYourself(person);
+			Console.WriteLine();
+
+			Person studentPerson = new StudentConcretePerson("Petr", "Petrov", 24, 3);
+			studentPerson.IntroduceYourself(studentPerson);
+			Console.WriteLine();
+
+			Person extramuralStudentPerson = new ExtramuralStudentConcretePerson("Ivan", "Ivanov", 20,2);
+			extramuralStudentPerson.IntroduceYourself(extramuralStudentPerson);
+
+			Console.ReadKey();
+		}
+	}
+}
