8461c5e6d7f8bf82f04509622a93d35c2055d485
OlegKlevzhic@gmail.com <OlegKlevzhic@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-17 12:20:54 +0000

    Added by the matrix calculator and comments.

--- trunk/OlegKlevzhic/My.Calculator/My.Calculator/ComplexNumbers/Calculator.cs
+++ trunk/OlegKlevzhic/My.Calculator/My.Calculator/ComplexNumbers/Calculator.cs
@@ -8,27 +8,40 @@ namespace My.Calculator.ComplexNumbers
 {
     public class Calculator : ICalculator<Complex<double>>
     {
-
+		/// <summary>
+		/// The method returns the sum of two complex numbers.
+		/// </summary>
+		/// <param name="a">The first term is complete.</param>
+		/// <param name="b">The second term is complete.</param>
         public Complex<double> Add(Complex<double> a, Complex<double> b)
         {
-            return new Complex<double>(a.re + b.re, a.im + b.im);
+            return new Complex<double>(a.Re + b.Re, a.Im + b.Im);
         }
 
+		/// <summary>
+		/// The method returns the difference of two complex numbers.
+		/// </summary>
         public Complex<double> Subtracht(Complex<double> a, Complex<double> b)
         {
-            return new Complex<double>(a.re - b.re, a.im - b.im);
+            return new Complex<double>(a.Re - b.Re, a.Im - b.Im);
         }
 
+		/// <summary>
+		/// The method returns the multiply of two complex numbers.
+		/// </summary>
         public Complex<double> Multiply(Complex<double> a, Complex<double> b)
         {
-            return new Complex<double>(a.re * b.re - a.im * b.im, a.re * b.im + a.im * b.re);
+            return new Complex<double>(a.Re * b.Re - a.Im * b.Im, a.Re * b.Im + a.Im * b.Re);
         }
 
+		/// <summary>
+		/// The method return the divide of two complex number.
+		/// </summary>
         public Complex<double> Divide(Complex<double> a, Complex<double> b)
         {
-            if (b.im != 0 || b.re != 0)
+            if (b.Im != 0 || b.Re != 0)
             {
-                return new Complex<double>((a.re * b.re + a.im * b.im) / (b.re * b.re + b.im * b.im), (a.im * b.re - a.re * b.im) / (b.re * b.re + b.im * b.im));
+                return new Complex<double>((a.Re * b.Re + a.Im * b.Im) / (b.Re * b.Re + b.Im * b.Im), (a.Im * b.Re - a.Re * b.Im) / (b.Re * b.Re + b.Im * b.Im));
             }
             else
             {
