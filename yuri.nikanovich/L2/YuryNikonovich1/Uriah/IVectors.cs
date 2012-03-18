acded49d509497e4f6187bbd3917a0dc983a401b
yury.nikonovich <yury.nikonovich@gmail.com>
2012-03-18 09:55:00 +0000

    second lab added

---
+++ yuri.nikanovich/L2/YuryNikonovich1/Uriah/IVectors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Uriah
+{
+    public interface IVectors
+    {
+        public IVectors Sum(IVectors firstVector, IVectors secondVector);
+        public IVectors Substruction(IVectors firstVector, IVectors secondVector);
+        public IVectors MultiplyOnNumber(IVectors firstVector, double lambda);
+        public double ScaleMultiply(IVectors firstVector, IVectors secondVector);
+
+    }
+}
