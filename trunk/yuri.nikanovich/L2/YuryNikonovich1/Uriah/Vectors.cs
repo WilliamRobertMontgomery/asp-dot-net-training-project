b50c4ab92caaa9f0bf8cc48af85ee6a0bda3dc4e
yury.nikonovich@gmail.com <yury.nikonovich@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-18 09:55:00 +0000

    second lab added

---
+++ trunk/yuri.nikanovich/L2/YuryNikonovich1/Uriah/Vectors.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Uriah
+{
+    public class Vectors : IVectors
+    {
+        public double X {get; set; }
+        public double Y {get; set; }
+        public double Z {get; set; }
+
+        public IVectors Sum(IVectors firstVector, IVectors secondVector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVectors Substruction(IVectors firstVector, IVectors secondVector)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVectors MultiplyOnNumber(IVectors firstVector, double lambda)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double ScaleMultiply(IVectors firstVector, IVectors secondVector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
