4f765eecc867dcdf51e0536a25869d4966d99fd4
filanovich.barys@gmail.com <filanovich.barys@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-19 20:19:03 +0000

    Added a solution Printer

---
+++ trunk/barys.filanovich/PrinterSolution/Printer/IPrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPrinters
+{
+    public interface IPrinter
+    {
+        void AddDoc(Document newDoc);
+        void Print();
+        void ReplaceCartrige(int capacityCartridge = 1000);
+        void Capacity();
+        void GetPrintTechnology();
+        void GetPrinterInfo();
+        void GetPrintQueue();
+        bool IsColor { get; }
+    }
+}
