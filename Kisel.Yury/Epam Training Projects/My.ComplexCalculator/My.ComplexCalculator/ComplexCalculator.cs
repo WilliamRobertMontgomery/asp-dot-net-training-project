806669a16e481c8b6cfced4d1c3ec1a0d7e8e216
Kisel.Yury <Kisel.Yury@gmail.com>
2012-03-20 13:20:59 +0000

    Added folder "Epam Training Projects"

---
+++ Kisel.Yury/Epam Training Projects/My.ComplexCalculator/My.ComplexCalculator/ComplexCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.ComplexCalculator
+{
+    public class ComplexCalculator : IComplexCalculations
+    {
+        
+                
+        public ComplexNumber Add(ComplexNumber a, ComplexNumber b)
+        {
+            return a + b;
+        }
+
+        public ComplexNumber Substract(ComplexNumber a, ComplexNumber b)
+        {
+            return a - b;
+        }
+
+        public ComplexNumber Multiply(ComplexNumber a, ComplexNumber b)
+        {
+            return a * b;
+        }
+
+        public ComplexNumber Divide(ComplexNumber a, ComplexNumber b)
+        {
+            if (b.Im != 0 || b.Re != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                throw new DivideByZeroException();
+            }
+        }
+    }
+}
