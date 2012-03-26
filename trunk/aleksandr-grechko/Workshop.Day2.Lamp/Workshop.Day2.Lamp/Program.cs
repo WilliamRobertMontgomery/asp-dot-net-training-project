797e686d3126fb9084162c72d9ae1005e7a23cf3
aleksandr.grechko@gmail.com <aleksandr.grechko@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-26 18:43:52 +0000

    Add Workshop.Day2.Lamp

---
+++ trunk/aleksandr-grechko/Workshop.Day2.Lamp/Workshop.Day2.Lamp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Workshop.Day2.Lamp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Lamp lamp = new Lamp();
+			lamp.TurnOnLight();
+			lamp.ReplaceBulb(30);
+			lamp.GetState();
+
+			Console.WriteLine();
+
+			DeskLamp deskLamp = new DeskLamp(25, Place.Chair, true);
+			deskLamp.GetState();
+			deskLamp.MoveDeskLamp(Place.Floor);
+			deskLamp.GetState();
+
+			Console.WriteLine();
+
+			LigthSource[] allLamps = { lamp, deskLamp };
+
+			foreach (LigthSource l in allLamps)
+			{
+				(l as Lamp).ReplaceBulb(50);
+				if (l is DeskLamp)
+				{
+					(l as DeskLamp).MoveDeskLamp(Place.Table);
+				}
+				l.TurnOffLight();
+				l.GetState();
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
