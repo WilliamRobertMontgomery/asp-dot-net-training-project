31fd2267ea1c0811d1e3f1ed4570cf7376c06570
Aleksander.Kolyada@gmail.com <Aleksander.Kolyada@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-03-27 07:27:39 +0000

    Day2.Workshop added

---
+++ trunk/aleksander.kolyada/Day2.Workshop/My.Ships/AircraftCarrier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Ships
+{
+    class AircraftCarrier:WarShip
+    {
+        //field
+        private int _numberOfAircrafts;
+
+        //property
+        public int NumberOfAircrafts
+        {
+            get { return _numberOfAircrafts; }
+            set { _numberOfAircrafts = value; }
+        }
+
+        //constructors
+        public AircraftCarrier()
+        {
+            Name = null;
+            NumberOfCrew = 0;
+            Draft = 0;
+            NumberOfAircrafts = 0;
+        }
+
+        public AircraftCarrier(string name, int numberOfCrew, int draft, int numberOfAircrafts)
+        {
+            this.Name = name;
+            this.NumberOfCrew = numberOfCrew;
+            this.Draft = draft;
+            this.NumberOfAircrafts = numberOfAircrafts;
+        }
+
+        //override methods
+        public override void sailTo(string s)
+        {
+            Console.WriteLine("Aircraft Carrier {0} with draft {1} ton sails to {2}. Number of crew - {3}. Number of aircrafts - {4}", Name, Draft, s, NumberOfCrew, NumberOfAircrafts);
+        }
+
+        public override void action()
+        {
+            Console.WriteLine("Provides air defense for maritime links.");
+        }
+
+    }
+}
---
+++ trunk/aleksander.kolyada/Day2.Workshop/My.Ships/WarShip.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Ships
+{
+    class WarShip:Ship
+    {
+        //field
+        private int _numberOfGuns;
+
+        //property
+        public int NumberOfGuns
+        {
+            get { return _numberOfGuns; }
+            set { _numberOfGuns = value; }
+        }
+
+        //constructors
+        public WarShip()
+        {
+            Name = null;
+            NumberOfCrew = 0;
+            Draft = 0;
+            NumberOfGuns = 0;
+        }
+
+        public WarShip(string name, int numberOfCrew, int draft, int numberOfGuns)
+        {
+            this.Name = name;
+            this.NumberOfCrew = numberOfCrew;
+            this.Draft = draft;
+            this.NumberOfGuns = numberOfGuns;
+        }
+
+        //override methods
+        public override void sailTo(string s)
+        {
+            Console.WriteLine("Warship {0} with draft {1} ton sails to {2}. Number of crew - {3} чел. Number of guns - {4}", Name, Draft, s, NumberOfCrew, NumberOfGuns);
+        }
+
+        public override void action()
+        {
+            Console.WriteLine("Takes part in the military exercises.");
+        }
+
+    }
+}
