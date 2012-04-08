fb4787c3a4bf620920689d30c6bba4d28c128a4e
andrei.shostik <Andrei.shostik@gmail.com>
2012-04-08 18:16:01 +0000

    added WorkShop.Day2.Technics

---
+++ andrei-shostik/WorkShop.Day2.Technics/Technics/Players/VideoPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Technics.Players
+{
+    internal class VideoPlayer : Player
+    {
+        public VideoPlayer()
+        {
+        }
+
+        public VideoPlayer(string manufacturer, double? cost)
+            : base(manufacturer, cost)
+        {
+        }
+
+        public VideoPlayer(PlayList playList)
+            : base(playList)
+        {
+        }
+
+        public VideoPlayer(VideoClip videoClip)
+            : base(videoClip)
+        {
+        }
+
+        public override bool IsOn()
+        {
+            Console.WriteLine(base.IsOn() ? "VideoPlayer {0} is on" : "VideoPlayer {0} is off", ID);
+            return base.IsOn();
+        }
+
+        public override void Play(PlayList playList)
+        {
+            Console.WriteLine("Play playlist");
+            base.Play(playList);
+        }
+
+        public override void Play(VideoClip videoClip)
+        {
+            Console.WriteLine("Play videoclip");
+            base.Play(videoClip);
+        }
+    }
+}
