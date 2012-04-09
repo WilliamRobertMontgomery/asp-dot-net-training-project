1cef046942be3687bcf792e155f63e6173cf02ec
YuraZhigarevich@gmail.com <YuraZhigarevich@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-04-09 20:06:47 +0000

    Added LabRab1 (BetaVersion) for code review

---
+++ trunk/YuraZhigarevich/LabRab1/WebApplication1/WebApplication1/FindDeclaration.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.IO;
+
+namespace WebApplication1
+{
+    public partial class FindDeclaration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line,ss;
+            ss = "";
+            var path = Server.MapPath("output.txt");
+            StreamReader f = new StreamReader(path);
+            while ((line = f.ReadLine()) != null)
+            {
+                ss = ss + line+"\n";
+                TextBox1.Text = ss;
+                //Label1.Text = ss;
+                counter++;
+            }
+            f.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+        }
+    }
+}
---
+++ trunk/YuraZhigarevich/LabRab1/WebApplication1/WebApplication1/main.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="main.aspx.cs" Inherits="WebApplication1.main" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+    <style type="text/css">
+        .style1
+        {
+            font-family: cursive;
+            font-size: x-large;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body style="text-align: center">
+    <form id="form1" runat="server">
+    <p>
+        &nbsp;</p>
+    <p class="style1">
+        С помощь нашего сайта слова превращаются в покупки!</p>
+    <p>
+        <asp:Image ID="Image1" runat="server" Height="190px" 
+            ImageUrl="~/Pictures/doska.jpg" Width="226px" />
+    </p>
+</form>
+</body>
+</html>
