c68557e9dd795e5ee6e668a2eb16f70378a4c213
YuraZhigarevich <YuraZhigarevich@gmail.com>
2012-04-09 20:06:47 +0000

    Added LabRab1 (BetaVersion) for code review

---
+++ YuraZhigarevich/LabRab1/WebApplication1/WebApplication1/AddDeclaration.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.IO;
+
+
+namespace WebApplication1
+{
+    public partial class AddDeclaration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           string s = TextBox1.Text;
+           var path = Server.MapPath("output.txt");
+           StreamWriter g = new StreamWriter(path, true);
+           g.WriteLine(s);
+           g.Close();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.Text = "";
+        }
+    }
+}
