2c558f4102bdfbfd5938f72d800c523c1f9b0ae9
eugene.novik.o <eugene.novik.o@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-04-17 11:39:39 +0000

    Added Day3.Workshop

---
+++ trunk/eugene.novik/Day3.Workshop.Matrix/Day3.Workshop.Matrix.Test/MatrixTest.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Day3.Workshop.Matrix;
+
+
+namespace Day3.Workshop.Matrix.Test
+{
+	[TestFixture]
+	public class MatrixTest
+	{
+
+		[Test]
+		public void Equals_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix c = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+
+			Assert.IsTrue(a.Equals(a));
+			Assert.IsTrue(a.Equals(b) == b.Equals(a));
+			Assert.IsTrue(a.Equals(b) && b.Equals(c));
+			Assert.IsFalse(a.Equals(null));
+
+			Assert.IsTrue(a.Equals(c));
+			c = new Matrix(new double[,] { { 0, 0 }, { 0, 0 } });
+			Assert.IsFalse(a.Equals(c));
+		}
+
+
+		[Test]
+		public void Not_Equals_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix d = new Matrix(new double[,] { { 10, 20 }, { 30, 40 } });
+			Matrix e = new Matrix(new double[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
+
+			Assert.IsFalse(a.Equals(d));
+			Assert.IsFalse(a.Equals(e));
+			Assert.IsTrue(a.Equals(d) == d.Equals(a));
+		}
+
+
+		[Test]
+		public void GetHashCode_Equal_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+
+			Assert.IsTrue(a.Equals(b));
+			Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+		}
+
+
+		[Test]
+		public void GetHashCode_Not_Equal_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 10, 20 }, { 30, 40 } });
+
+			Assert.IsFalse(a.Equals(b));
+			Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+		}
+
+
+		[Test]
+		public void Add_Equal_Dimensions_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 10, 20 }, { 30, 40 } });
+			Matrix c = new Matrix(new double[,] { { 11, 22 }, { 33, 44 } });
+
+			Assert.AreEqual(Matrix.Add(a, b), c);
+			Assert.AreEqual(a + b, c);
+			Assert.AreEqual(b + a, c);
+		}
+
+
+		[Test]
+		public void Add_Not_Equal_Dimensions_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 10, 20, 30 }, { 40, 50, 60 } });
+
+			Assert.AreEqual(Matrix.Add(a, b), null);
+			Assert.AreEqual(a + b, null);
+			Assert.AreEqual(b + a, null);
+		}
+
+
+		[Test]
+		public void Multiply_Equal_Dimension_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 10, 20 }, { 30, 40 } });
+			Matrix c = new Matrix(new double[,] { { 70, 100 }, { 150, 220 } });
+
+			Assert.AreEqual(Matrix.Multiply(a, b), c);
+			Assert.AreEqual(a * b, c);
+			Assert.AreEqual(b * a, c);
+		}
+		
+
+		[Test]
+		public void Multiply_Not_Equal_Dimension_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 1, 2 }, { 4, 5 }, { 7, 8 } });
+			Matrix c = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+
+			Assert.AreEqual(Matrix.Multiply(a, b), null);
+			Assert.AreEqual(a * b, null);
+
+			Assert.AreEqual(Matrix.Multiply(a, c), null);
+			Assert.AreEqual(Matrix.Multiply(b, c), null);
+		}
+
+
+		[Test]
+		public void MultiplyByScalar_Test()
+		{
+			Matrix a = new Matrix(new double[,] { { 1, 2 }, { 3, 4 } });
+			Matrix b = new Matrix(new double[,] { { 10, 20 }, { 30, 40 } });
+			double d = 10.0;
+
+			Assert.AreEqual(Matrix.MultiplyByScalar(a, d), b);
+			Assert.AreEqual(a * d, b);
+			Assert.AreEqual(d * a, b);
+		}
+
+
+		[Test]
+		public void Index_Get_Test()
+		{
+			Matrix a = new Matrix(new double[2, 2] { { 1, 2 }, { 3, 4 } });
+
+			Assert.AreEqual(a[0, 0], 1);
+			Assert.AreEqual(a[0, 1], 2);
+			Assert.AreEqual(a[1, 0], 3);
+			Assert.AreEqual(a[1, 1], 4);
+		}
+
+
+		[Test]
+		public void Index_Set_Test()
+		{
+			Matrix a = new Matrix(new double[2, 2]);
+
+			a[0, 0] = 10;
+			a[0, 1] = 20;
+			a[1, 0] = 30;
+			a[1, 1] = 40;
+
+			Assert.AreEqual(a[0, 0], 10);
+			Assert.AreEqual(a[0, 1], 20);
+			Assert.AreEqual(a[1, 0], 30);
+			Assert.AreEqual(a[1, 1], 40);
+		}
+
+
+		[Test]
+		public void Index_Out_Of_Range_Test()
+		{
+			Matrix a = new Matrix(new double[2, 2] { { 1, 2 }, { 3, 4 } });
+
+			try
+			{
+				a[2, 2] = 10.0;
+			}
+			catch (Exception e)
+			{
+				Assert.AreEqual(typeof(System.IndexOutOfRangeException), e.GetType());
+			}
+		}
+
+	}
+}
---
+++ trunk/eugene.novik/Day3.Workshop.Matrix/Day3.Workshop.Matrix/Matrix.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day3.Workshop.Matrix
+{
+	public class Matrix
+	{
+		private double[,] items;
+
+		private int rows;
+		private int columns;
+
+
+		public int Rows
+		{
+			get { return rows; }
+		}
+		public int Columns
+		{
+			get { return columns; }
+		}
+
+
+		public Matrix(double[,] arr)
+		{
+			rows = arr.GetLength(0);
+			columns = arr.GetLength(1);
+
+			items = new double[rows, columns];
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < columns; j++)
+				{
+					items[i, j] = arr[i, j];
+				}
+			}
+		}
+
+
+		public double this[int index1, int index2]
+		{
+			get
+			{
+				CheckIndex(index1, index2);
+				return items[index1, index2];
+			}
+			set
+			{
+				CheckIndex(index1, index2);
+				items[index1, index2] = value;
+			}
+		}
+
+		public bool CheckIndex(int index1, int index2)
+		{
+			if ((index1 < 0 || index1 >= rows) || (index1 < 0 || index1 >= rows))
+			{
+				throw new IndexOutOfRangeException("Index was outside the bounds of the matrix");
+			}
+			return true;
+		}
+
+
+		public static Matrix Add(Matrix a, Matrix b)
+		{
+
+			if (a.Rows != b.Rows || a.Columns != b.Columns)
+			{
+				return null;
+			}
+
+			Matrix resultMatrix = new Matrix(new double[a.Columns, a.Rows]);
+
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < a.Columns; j++)
+				{
+					resultMatrix.items[i, j] = a.items[i, j] + b.items[i, j];
+				}
+			}
+			return resultMatrix;
+		}
+
+		public static Matrix operator +(Matrix a, Matrix b)
+		{
+			return Add(a, b);
+		}
+
+
+		public static Matrix MultiplyByScalar(Matrix a, double b)
+		{
+			Matrix resultMatrix = new Matrix(new double[a.Columns, a.Rows]);
+
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < a.Columns; j++)
+				{
+					resultMatrix.items[i, j] = a.items[i, j] * b;
+				}
+			}
+			return resultMatrix;
+		}
+
+		public static Matrix operator *(Matrix a, double b)
+		{
+			return MultiplyByScalar(a, b);
+		}
+
+		public static Matrix operator *(double b, Matrix a)
+		{
+			return MultiplyByScalar(a, b);
+		}
+
+
+
+		public static Matrix Multiply(Matrix a, Matrix b)
+		{
+			if (a.Columns != b.Rows)
+			{
+				return null;
+			}
+
+			Matrix resultMatrix = new Matrix(new double[a.Rows, b.Columns]);
+
+			for (int i = 0; i < a.Rows; i++)
+			{
+				for (int j = 0; j < b.Columns; j++)
+				{
+					resultMatrix.items[i, j] = 0;
+					for (int k = 0; k < a.Columns; k++)
+					{
+						resultMatrix.items[i, j] += a.items[i, k] * b.items[k, j];
+					}
+				}
+			}
+
+			return resultMatrix;
+		}
+
+		public static Matrix operator *(Matrix a, Matrix b)
+		{
+			return Multiply(a, b);
+		}
+
+
+		public override bool Equals(object obj)
+		{
+			bool result = false; 
+
+			Matrix other = obj as Matrix;
+
+			if (other != null)
+			{
+				if (other.Rows == Rows && other.Columns == Columns)
+				{
+					result = true;
+					for (int i = 0; i < Rows; i++)
+					{
+						for (int j = 0; j < Columns; j++)
+						{
+							if (items[i, j] != other.items[i, j])
+							{
+								result = false;
+							}
+						}
+					}
+				}
+			}
+			return result;
+		}
+
+		public override int GetHashCode()
+		{
+			return this.ToString().GetHashCode();
+		}
+
+		public static bool operator ==(Matrix a, Matrix b)
+		{
+			return Object.Equals(a, b);
+		}
+
+		public static bool operator !=(Matrix a, Matrix b)
+		{
+			return !Object.Equals(a, b);
+		}
+
+
+		public override string ToString()
+		{
+			StringBuilder str = new StringBuilder();
+
+			for (int i = 0; i < Rows; i++)
+			{
+				for (int j = 0; j < Columns; j++)
+				{
+					str.AppendFormat("{0}\t", items[i, j]);
+				}
+				str.AppendFormat("\n");
+			}
+
+			return str.ToString();
+		}
+
+	}
+}
