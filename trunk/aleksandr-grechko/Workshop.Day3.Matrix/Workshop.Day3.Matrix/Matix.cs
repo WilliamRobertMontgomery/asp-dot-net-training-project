5c904aa52680d694d4f80319df2bab12eaa76d9b
aleksandr.grechko@gmail.com <aleksandr.grechko@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-04-18 13:41:04 +0000

    Workshop.Day3.Matrix change

--- trunk/aleksandr-grechko/Workshop.Day3.Matrix/Workshop.Day3.Matrix/Matix.cs
+++ trunk/aleksandr-grechko/Workshop.Day3.Matrix/Workshop.Day3.Matrix/Matix.cs
@@ -195,17 +195,5 @@ namespace Workshop.Day3.Matrix
 
 	}
 
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Matrix m1 = new Matrix(5, 5, 10);
-			
-			Console.WriteLine(m1);
 
-			Console.WriteLine(new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
-
-			Console.Read();
-		}
-	}
 }
