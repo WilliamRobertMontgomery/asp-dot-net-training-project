e3d087ac5c59ab107060839f93d892466b9519dd
EdikTrutsik <EdikTrutsik@gmail.com>
2012-04-19 02:24:48 +0000

    Added workshop Day 7. My.Reflection

---
+++ Eduard.Trutsik/My.Reflection/My.Complex/IComplex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My.Complex
+{
+    interface IComplex
+    {
+        Complex Sum(Complex number);
+        Complex Subtract(Complex number);
+        Complex Multiply(Complex number);
+        Complex Divide(Complex number);
+        double Module();
+    }
+}
