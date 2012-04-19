1f99d6474c57abaf41e9a77c95c6a950d9c0fdde
EdikTrutsik@gmail.com <EdikTrutsik@gmail.com@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-04-19 02:24:48 +0000

    Added workshop Day 7. My.Reflection

---
+++ trunk/Eduard.Trutsik/My.Reflection/My.Complex.Reflection/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace My.Complex.Reflection
+{
+	class Program
+	{
+		private const string nameAssembly = "My.Complex.dll"; 
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Автор: Труцик Эдуард.");
+			Console.WriteLine("Рефлексия библиотеки {0}",nameAssembly);
+			Console.WriteLine();
+			reflectionInfoAndRun();
+			
+		}
+		static void reflectionInfoAndRun()
+		{
+			Assembly assmembly = Assembly.LoadFrom(nameAssembly);
+			AssemblyName assemblyName = assmembly.GetName();
+			Console.WriteLine("Полное имя: {0}", assemblyName.FullName);
+			Console.WriteLine("Версия сборки: {0}", assemblyName.Version);		
+			Console.WriteLine();
+			Type[] allType = assmembly.GetTypes();
+			foreach (Type type in allType)
+			{
+				Console.WriteLine("Найден тип:"+type.Name);
+				ConstructorInfo[] allConstructorInfo = type.GetConstructors();
+				foreach (ConstructorInfo constr in allConstructorInfo)
+				{
+					ParameterInfo[] allParameter = constr.GetParameters();
+					Console.Write("Найден конструктор: "+type.Name + "(");
+					foreach (ParameterInfo parameter in allParameter)
+					{
+						Console.Write(parameter.ParameterType.Name + " "+parameter.Name+"; ");
+					}
+					Console.WriteLine(");");
+					if (allParameter.Length == 2)
+					{
+						object[] constrParam = new object[2];
+						constrParam[0] = 2;
+						constrParam[1] = 5;
+						Console.WriteLine();
+						Console.WriteLine("Вызываем конструктор с параметрами: re={0}, im={1}",constrParam[0],constrParam[1]);
+						object reflectObject = constr.Invoke(constrParam);
+						MethodInfo[] allMethods = type.GetMethods();
+						foreach (MethodInfo metod in allMethods)
+						{
+							Console.WriteLine("Обнаружен метод:"+ metod.Name);
+							if (metod.Name == "Sum")
+							{
+								ParameterInfo[] allMetodParameters = metod.GetParameters();
+								Console.WriteLine();
+								Console.Write("Метод Sum(");
+								foreach (ParameterInfo metodParameter in allMetodParameters)
+								{
+									Console.Write(metodParameter.ParameterType.Name+" "+metodParameter.Name+"; ");
+								}
+								Console.WriteLine(");");
+								if (allMetodParameters.Length == 1)
+								{
+									Console.WriteLine("Вызов метода Sum с одним входным параметром.");
+									if (allMetodParameters[0].ParameterType.Name == type.Name)
+									{
+										Console.WriteLine("Вызов метода Sum(this)");
+										object[] objParam = new object[1];
+										objParam[0]=reflectObject;
+										Console.WriteLine("Sum(this)="+metod.Invoke(reflectObject, objParam));
+										Console.WriteLine();
+									}
+								}
+							}
+							if (metod.Name == "ToString")
+							{
+								Console.WriteLine();
+								Console.WriteLine("Вызов метода " + metod.Name);
+								Console.WriteLine(metod.Invoke(reflectObject, null));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ trunk/Eduard.Trutsik/My.Reflection/My.Complex/Complex.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace My.Complex
+{
+    public class Complex:IComplex
+    {
+        public double re;
+        public double im;
+
+        public Complex(double re, double im)
+        {
+            this.re = re;
+            this.im = im;
+        }
+
+        public Complex Sum(Complex number)
+        {
+            return new Complex(this.re + number.re, this.im + number.im);
+        }
+
+        public Complex Subtract(Complex number)
+        {
+            return new Complex(this.re - number.re, this.im - number.im);
+        }
+
+        public Complex Multiply(Complex number)
+        {
+            return new Complex(this.re * number.re - this.im * number.im, this.re * number.im + this.im * number.re);
+        }
+
+        public Complex Divide(Complex number)
+        {
+			if (number.re == 0 && number.im == 0)
+			{
+				throw new DivideByZeroException();
+			}            
+            try
+            {
+                return new Complex((this.re * number.re + this.im * number.im) / (number.re * number.re + number.im * number.im), (this.im * number.re - this.re * number.im) / (number.re * number.re + number.im * number.im));
+            }
+            catch (DivideByZeroException e)
+            {
+                Trace.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+
+        public double Module()
+        {
+            return Math.Sqrt(re * re + im * im);
+        }
+
+        public bool Equals(Complex number)
+        {
+            if (number != null)
+            {
+                if (this.im == number.im && this.re == number.re)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            if (im > 0)
+            {
+                return re + "+" + im + "i";
+            }
+            else if (im <0)
+            {
+                return re + "" + im + "i";
+            }
+            else if (im == 0)
+            {
+                return re.ToString();
+            }
+            else if (re == 0)
+            {
+                return im + "i";
+            }
+            else if (re == 0 && im == 0)
+            {
+                return "0";
+            }
+            else
+            {
+                return "null";
+            }
+        }
+
+		public override int GetHashCode()
+		{
+			return re.GetHashCode() ^ im.GetHashCode();
+		}
+    }
+}
