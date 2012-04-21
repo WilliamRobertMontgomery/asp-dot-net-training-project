139b7dd565327d20003a203ad19be785099111a6
filanovich.barys <filanovich.barys@gmail.com>
2012-04-21 18:59:21 +0000

    Modified my Lab#1

--- barys.filanovich/Lab#1/CarRental/CarRental/BusinessLogic/CarRentalSystem.cs
+++ barys.filanovich/Lab#1/CarRental/CarRental/BusinessLogic/CarRentalSystem.cs
@@ -3,94 +3,212 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CarRental.DataAccess;
-using CarRental.BusinessLogic;
+using CarRental.Entities;
 
-namespace CarRental.Entities
+namespace CarRental.BusinessLogic
 {
     public class CarRentalSystem
     {
-        Client[] clients;
-        Client selectedClient;
-        Car[] cars;
+        private const int timeout = 5;      //minutes
+        private Car selectedCar = null;
+        private Client selectedClient = null;
+
+        private IRepository<Car> _carRepository = new CarRepository();
+        private IRepository<Client> _clientRepository = new ClientRepository();
 
         public CarRentalSystem()
         {
-            cars = CarRepository.GetCars();
-            clients = ClientRepository.GetClients();
-            Reset();
+            this.ResetCarStatus();
         }
 
-        public Client Client
+        private void ResetCarStatus()
         {
-            get
+            foreach(Car car in this.GetCars())
             {
-                if (selectedClient != null)
-                    return selectedClient;
-                return null;
+                try
+                {
+                    if ((DateTime.Now - DateTime.Parse(car.DateOfLease)).Minutes > timeout)
+                    {
+                        car.Client = "free";
+                        car.DateOfLease = null;
+                        _carRepository.Save(car);
+                    }
+                }
+                catch(FormatException)
+                {
+                    continue;
+                }
             }
         }
 
-        public void SelectClient(int num)
+        private Car[] GetCars()
         {
-            if (num >= 0 && num < clients.Length)
-                selectedClient = clients[num];
+            return _carRepository.Read();
+        }
+
+        private Client[] GetClients()
+        {
+            return _clientRepository.Read();
+        }
+
+        public void AddCar(Car car)
+        {
+            if (!this.CarAvailable(car.Id))
+            {
+                _carRepository.Save(car);
+                Console.WriteLine("The car was added.");
+            }
             else
-                Console.WriteLine("Client error!");
+            {
+                Console.WriteLine("This ID ({0}) is used.", car.Id);
+            }
+        }
+
+        private bool CarAvailable(string id)
+        {
+            foreach (Car car in this.GetCars())
+            {
+                if (string.Compare(car.Id, id, false) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void AddClient(Client client)
+        {
+            if (this.IdClientIsOK(client.Id))
+            {
+                _clientRepository.Save(client);
+                Console.WriteLine("The client was added.");
+            }
+            else
+            {
+                Console.WriteLine("This ID ({0}) is used.", client.Id);
+            }
+        }
+
+        private bool IdClientIsOK(string id)
+        {
+            foreach (Client client in this.GetClients())
+            {
+                if (string.Compare(client.Id, id) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public void ShowClients()
         {
-            foreach (Client client in clients)
+            foreach (Client client in this.GetClients())
+            {
                 Console.WriteLine(client);
+                Console.WriteLine();
+            }
         }
 
         public void ShowCars()
         {
-            foreach (Car car in cars)
-                Console.WriteLine(car);
-        }
-
-        public void GetCar(string brand,Client client)
-        {
-            for (int i = 0; i < cars.Length; i++)
+            this.ResetCarStatus();
+            foreach (Car car in this.GetCars())
             {
-                if (string.Compare(cars[i].BrandName, brand, true)==0)
-                {
-                    if (cars[i].Status == "free")
-                    {
-                        cars[i].Status = client.Id;
-                        cars[i].DateOfLease = DateTime.Now.ToString();
-                        CarRepository.Save(cars[i]);
-                    }
-                    else
-                    {
-                        int timeOut = 10-(DateTime.Now - DateTime.Parse(cars[i].DateOfLease)).Minutes;
-
-                        Console.WriteLine("This car will be free after {0} min",
-                            timeOut);
-                    }
-                }
+                Console.WriteLine(car);
+                Console.WriteLine();
             }
         }
 
-        public void Reset()
+        private Client SelectClient(string id)
         {
-            for (int i = 0; i < cars.Length; i++)
+            foreach (Client client in this.GetClients())
             {
-                if (string.Compare(cars[i].Status, "free", true) == 0)
+                if (string.Compare(client.Id, id) == 0)
                 {
-                    continue;
+                    return client;
                 }
+            }
+            Console.WriteLine("Invalid ID of Client!");
+            return null;
+        }
 
-                int timeOut = 10-(DateTime.Now - DateTime.Parse(cars[i].DateOfLease)).Minutes;
-                if (timeOut <= 0)
+        private Car SelectCar(string id)
+        {
+            foreach (Car car in this.GetCars())
+            {
+                if (string.Compare(car.Id, id, true) == 0)
                 {
-                    cars[i].Status = "free";
-                    cars[i].DateOfLease = null;
-                    CarRepository.Save(cars[i]);
-                    cars = CarRepository.GetCars();
-                    clients = ClientRepository.GetClients();
+                    return car;
                 }
+            }
+            Console.WriteLine("Invalid ID of Car!");
+            return null;
+        }
+
+        public void GetCar(string clientId, string carId)
+        {
+            selectedClient = this.SelectClient(clientId);
+            selectedCar =  this.SelectCar(carId);
+
+            this.ResetCarStatus();
+            if (selectedCar != null & selectedClient != null)
+            {
+                if (this.CarIsFree(selectedCar))
+                {
+                    selectedCar.Client = selectedClient.Id;
+                    selectedCar.DateOfLease = DateTime.Now.ToString();
+                    _carRepository.Save(selectedCar);
+                    Console.WriteLine(selectedCar.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("This car is used.");
+                }
+            }
+
+            ResetSelected();
+        }
+
+        private bool CarIsFree(Car car)
+        {
+            if (car.DateOfLease == string.Empty)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void ResetSelected()
+        {
+            selectedCar = null;
+            selectedClient = null;
+        }
+
+        public void RemoveClient(string id)
+        {
+            if (!this.IdClientIsOK(id))
+            {
+                _clientRepository.Remove(this.SelectClient(id));
+            }
+            else
+            {
+                Console.WriteLine("This ID of client is invalid!");
+            }
+        }
+
+        public void RemoveCar(string id)
+        {
+            if (this.CarAvailable(id))
+            {
+                _carRepository.Remove(this.SelectCar(id));
+            }
+            else
+            {
+                Console.WriteLine("This ID of car is invalid!");
             }
         }
     }
