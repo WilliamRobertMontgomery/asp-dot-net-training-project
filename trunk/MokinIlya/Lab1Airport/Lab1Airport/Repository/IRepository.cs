07384a0fe54026416c93b81a3c54a95f4e83cc05
ilia.mokin <ilia.mokin@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-05-01 11:45:02 +0000

    Added Lab1Airport with LinqToSql

--- trunk/MokinIlya/Lab1Airport/Lab1Airport/Repository/IRepository.cs
+++ trunk/MokinIlya/Lab1Airport/Lab1Airport/Repository/IRepository.cs
@@ -7,31 +7,11 @@ namespace Lab1Airport
 {
     interface IRepository<T>
     {
-        /// <summary>
-        /// Function get all items
-        /// </summary>
-        /// <returns>IQueryable</returns>
-        IQueryable<T> GetAll();
-
-        /// <summary>
-        /// Save or update item
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <returns>if item is saved return true</returns>
-        bool Save(T entity);
-
-        /// <summary>
-        /// Function delete item by id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>if item is delete return true</returns>
-        bool Delete(int id);
-
-        /// <summary>
-        /// Function delete item by 
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <returns></returns>
-        bool Delete(T entity);
+        T GetElement(int id);
+        IEnumerable<T> GetAll();
+        void AddElement(T element);
+        void UpdateElement(T element);
+        void DeleteElement(int id);
+        void DeleteElement(T element);
     }
 }
