ff7b92cee836081d644a1b961a94cc10a7ee0bcf
filanovich.barys <filanovich.barys@gmail.com>
2012-05-03 08:16:28 +0000

    Added the missing files.

---
+++ barys.filanovich/Lab#1/CarRental/CarRental/DataAccess/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarRental.DataAccess
+{
+    interface IRepository<T>
+    {
+        T[] Read();
+        void Save(T item);
+        void Remove(T item);
+    }
+}
