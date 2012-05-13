bb09861e7ff9cdf2b427d131f638afbf333c212e
ilia.mokin <ilia.mokin@71af1dd4-c80a-3023-7950-0f1aad32ed4e>
2012-05-13 14:18:20 +0000

    added lab4

---
+++ MokinIlya/Lab4.Aiport.MVC/Lab4.Mvc.Aiport/DataAccess/Plains.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab4.Mvc.Aiport.Models;
+using Lab4.Mvc.Aiport.Repository;
+
+namespace Lab4.Mvc.Aiport.DataAccess
+{
+    public class DataAccessPlains : IRepository<Plains>
+    {
+        private DBAiportDataContext _context;
+
+        public DataAccessPlains()
+        {
+            _context = new DBAiportDataContext();
+        }
+
+        public Plains GetElement(int id)
+        {
+            return _context.Plains.SingleOrDefault(m => m.CodePlane == id);
+        }
+
+        public IEnumerable<Plains> GetAll()
+        {
+            return _context.Plains.AsEnumerable<Plains>();
+        }
+
+        public void AddElement(Plains element)
+        {
+            _context.Plains.InsertOnSubmit(element);
+            _context.SubmitChanges();
+        }
+
+        public void UpdateElement(Plains element)
+        {
+            Plains updateElement = GetElement(element.CodePlane);
+            if (updateElement != null)
+            {
+                updateElement.NumberOfSeats = element.NumberOfSeats;
+                _context.SubmitChanges();
+            }
+        }
+
+        public void DeleteElement(int id)
+        {
+            Plains element = GetElement(id);
+            _context.Plains.DeleteOnSubmit(element);
+            _context.SubmitChanges();
+        }
+
+        public void DeleteElement(Plains element)
+        {
+            DeleteElement(element.CodePlane);
+        }
+    }
+}
